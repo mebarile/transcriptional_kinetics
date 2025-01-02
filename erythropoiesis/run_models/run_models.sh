d2cf8df17f3367b5da7040c0d3e6800820d004ab
mebarile <58853086+mebarile@users.noreply.github.com>
2025-01-02 14:56:10 +0000

    Update run_models.sh

--- erythropoiesis/run_models/run_models.sh
+++ erythropoiesis/run_models/run_models.sh
@@ -1,6 +1,6 @@
 # run this line in the command line, changing the input: 
 # gene_number: taken from a list
-# reg: regulation parameter
+# reg: regularization parameter
 # pop1: usually 1 unless you have more than 2 conditions
 # pop2: usually 2 unless you have more than 2 conditions
 
